c2ff118cdc00178a1261b21449ec74ac202fa87e
vprathap21 <211501064@rajalakshmi.edu.in>
2024-01-11 07:59:33 +0530

    episode - 1

---
+++ App.js
@@ -0,0 +1,8 @@
+const parent = React.createElement("div", {id:"parent"},
+React.createElement("div",{id:"child"},
+[React.createElement("h1",{},"I'm an h1 tag"),
+React.createElement("h2",{},"I'm an h2 tag")]
+)
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
---
+++ README.md
@@ -0,0 +1,1 @@
+# Nmaste React 
