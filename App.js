001a7ebd65edbe1782630ccca7cdfd0adab04431
vprathap21 <211501064@rajalakshmi.edu.in>
2024-01-11 10:38:56 +0530

    episode - 2

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Corrected import statement
+
 const parent = React.createElement("div", {id:"parent"},
-React.createElement("div",{id:"child"},
-[React.createElement("h1",{},"I'm an h1 tag"),
-React.createElement("h2",{},"I'm an h2 tag")]
-)
+    React.createElement("div",{id:"child"},
+        [React.createElement("h1",{},"I'm an h1 tag"),
+        React.createElement("h2",{},"I'm an h2 tag")]
+    )
 );
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
+
